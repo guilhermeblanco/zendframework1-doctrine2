d800c9c4af922cfe5fa316ed4c7a40c2da136284
Koji Ando <miau.jp@gmail.com>
2011-08-08 03:20:46 +0900

    Fixed paths to be consistent with each other.

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -4,7 +4,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
     public function _initAutoloaderNamespaces()
     {
-        require_once APPLICATION_PATH . '/../library/Doctrine/Common/ClassLoader.php';
+        require_once APPLICATION_PATH . '/../library/vendor/Doctrine/lib/vendor/doctrine-common/lib/Doctrine/Common/ClassLoader.php';
 
         $autoloader = \Zend_Loader_Autoloader::getInstance();
         $fmmAutoloader = new \Doctrine\Common\ClassLoader('Bisna');
